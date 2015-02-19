9bc27567b3a62127635975acc1fb559b47491bf9
Matthew Heon <mheon@redhat.com>
2015-02-19 11:22:45 -0500

    Update configuration file syntax to be more like LXC
    Signed-off-by: Matthew Heon <mheon@redhat.com>

--- daemon/config.go
+++ daemon/config.go
@@ -1,9 +1,11 @@
 package daemon
 
 import (
-	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net"
+	"strconv"
+	"strings"
 
 	"github.com/docker/docker/daemon/networkdriver"
 	"github.com/docker/docker/opts"
@@ -111,7 +113,104 @@ func parseSeccompConfig(path string) (seccomp.SeccompConfig, error) {
 		return config, err
 	}
 
-	err = json.Unmarshal(contents, &config)
+
+	lines := strings.Split(string(contents), "\n")
+
+	switch strings.ToLower(lines[0]) {
+	case "whitelist":
+		config.Whitelist = true;
+	case "blacklist":
+		config.Whitelist = false;
+	default:
+		return config, fmt.Errorf("Error on line 1 of Seccomp config: Config file must start with WHITELIST or BLACKLIST!")
+	}
+
+	// Remove the first line
+	lines = lines[1:]
+
+	// Parse each syscall
+	for i, line := range lines {
+		if len(line) == 0 {
+			// Skip empty lines
+			continue
+		}
+
+		// Do we have arguments?
+		if strings.Index(line, "[") != -1 {
+			// We have arguments, parse them
+			argsStartIndex := strings.Index(line, "[")
+			argsEndIndex := strings.Index(line, "]")
+			if argsEndIndex == -1 {
+				return config, fmt.Errorf("Error on line %d of Seccomp config: Arguments list must be terminated by ]", (i + 2))
+			}
+
+			// Get arguments and syscall substrings
+			arguments := line[argsStartIndex+1:argsEndIndex]
+			syscall := strings.TrimRight(line[0:argsStartIndex], " ")
+			if len(syscall) == 0 {
+				return config, fmt.Errorf("Error on line %d of Seccomp config: Must provide name of syscall to block!", (i + 2))
+			}
+
+			blockedCall := seccomp.BlockedSyscall { Name: syscall }
+
+			// Split the arguments list, which should be semicolon-separated
+			argumentsList := strings.Split(arguments, ";")
+
+			for _, arg := range argumentsList {
+				// If the argument is empty, continue
+				if len(arg) == 0 {
+					continue
+				}
+
+				// Each argument is wrapped in parens
+				argNoParens := arg[1:len(arg)-1]
+
+				// Each will have 3 or 4 comma-separated fields
+				fields := strings.Split(argNoParens, ",")
+				if len(fields) < 3 || len(fields) > 4 {
+					return config, fmt.Errorf("Error on line %d of Seccomp config: Argument restrictions must have an argument number, an operator, and one or two values", (i + 2))
+				}
+
+				// Field 1 is the argument number
+				argNum, err := strconv.Atoi(fields[0])
+				if err != nil {
+					return config, fmt.Errorf("Error on line %d of Seccomp config: Could not convert argument number to integer: %s", (i + 2), err)
+				} else if argNum < 0 {
+					return config, fmt.Errorf("Error on line %d of Seccomp config: Argument number cannot be negative!", (i + 2))
+				}
+
+				// Field 3 (and optionally 4) are 64-bit ints
+				valOne, err := strconv.ParseInt(fields[2], 0, 64)
+				if err != nil {
+					return config, fmt.Errorf("Error on line %d of Seccomp config: Could not convert operand 1 to integer: %s", (i + 2), err)
+				}
+
+				var valTwo int64
+				if len(fields) == 4 {
+					valTwo, err = strconv.ParseInt(fields[2], 0, 64)
+					if err != nil {
+						return config, fmt.Errorf("Error on line %d of Seccomp config: Could not convert operand 2 to integer: %s", (i + 2), err)
+					}					
+				} else {
+					valTwo = 0
+				}
+
+				blockedCall.Conditions = append(blockedCall.Conditions, seccomp.SyscallCondition {
+					Argument: uint(argNum),
+					Operator: fields[1],
+					ValueOne: uint64(valOne),
+					ValueTwo: uint64(valTwo),
+				})
+			}
+
+			config.Syscalls = append(config.Syscalls, blockedCall)
+		} else {
+			// Assume the entire line is a syscall name
+			config.Syscalls = append(config.Syscalls, seccomp.BlockedSyscall{ Name: line })
+		}
+	}
+
+	config.Enable = true
 
 	return config, err
 }
